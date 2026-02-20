d7be04419617feb914f08f043d964c741ab7b3fa
altwebsites <alexgamer.roblox.test@gmail.com>
2026-02-20 18:25:48 +0200

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,8 @@
+-- Test Script
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "Script Loaded!";
+    Text = "Your GitHub loadstring is working!";
+    Duration = 5;
+})
+
+print("Success! Script fetched from GitHub.")
